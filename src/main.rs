49daa9f9b6790d28a58a7962e69150c0b4b38d78
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-04-17 18:19:10 +0400

    nannou boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use nannou::prelude::*;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .loop_mode(LoopMode::Rate {
+            update_interval: Duration::from_secs_f32(1. / 60.),
+        })
+        .run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(_app: &App, _model: &Model, frame: Frame) {
+    frame.clear(SKYBLUE);
 }
