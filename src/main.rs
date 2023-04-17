904f419bce74b52ba5f93153c1924b5e3023ca23
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-04-17 23:22:24 +0400

    basic circle renderer

--- src/main.rs
+++ src/main.rs
@@ -25,29 +25,49 @@ struct Model {
 fn model(_app: &App) -> Model {
     Model {
         image: DynamicImage::new_rgb8(WIN_WIDTH as u32, WIN_HEIGHT as u32),
-        position: Vec3::new(0., 0., 50.),
+        position: Vec3::new(WIN_WIDTH as f32 / 2., WIN_HEIGHT as f32 / 2., 200.),
         radius: 50.,
     }
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
+fn update(_app: &App, model: &mut Model, update: Update) {
     let mut rng = rand::thread_rng();
+    let circle = &mut model.position;
+    circle.x -= 5. * update.since_last.as_secs_f32();
 
     for y in 0..WIN_HEIGHT {
         for x in 0..WIN_WIDTH {
-            let r = (x as f32 / WIN_WIDTH as f32 * 255.) as u8;
-            let g = (y as f32 / WIN_HEIGHT as f32 * 255.) as u8;
-            let b = 130;
+            let pos = Vec3::new(x as f32, y as f32, 0.);
+            let dir = Vec3::Z;
 
-            model
-                .image
-                .put_pixel(x as u32, y as u32, Rgba::<u8>([r, g, b, 255]));
+            let a = dir.x * dir.x + dir.y * dir.y + dir.z * dir.z;
+            let b = 2. * pos.x * dir.x + 2. * pos.y * dir.y + 2. * pos.z * dir.z
+                - 2. * dir.x * circle.x
+                - 2. * dir.y * circle.y
+                - 2. * dir.z * circle.z;
+            let c = pos.x * pos.x + pos.y * pos.y + pos.z * pos.z
+                - 2. * pos.x * circle.x
+                - 2. * pos.y * circle.y
+                - 2. * pos.z * circle.z
+                + circle.x * circle.x
+                + circle.y * circle.y
+                + circle.z * circle.z
+                - model.radius * model.radius;
+
+            let det = b * b - 4. * a * c;
+
+            let color = if det >= 0. {
+                Rgba::<u8>([255, 255, 255, 255])
+            } else {
+                Rgba::<u8>([0, 0, 0, 255])
+            };
+
+            model.image.put_pixel(x as u32, y as u32, color);
         }
     }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    frame.clear(BLACK);
     let texture = Texture::from_image(app, &model.image);
     let draw = app.draw();
     draw.texture(&texture).finish();
