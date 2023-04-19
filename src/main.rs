46a609287f4db130945a1f67e3d39469f1451353
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-04-19 18:13:46 +0400

    shape trait

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use nannou::{
-    image::{DynamicImage, GenericImage, Rgb, Rgba},
+    image::{DynamicImage, GenericImage, Rgba},
     prelude::*,
     wgpu::Texture,
 };
-use ray::{ray_sphere_collision, Sphere};
+use ray::{HitInfo, Shape, Sphere};
 
 mod ray;
 
@@ -23,23 +23,33 @@ struct Model {
     image: DynamicImage,
     fov: f32,
     lighting_direction: Vec3,
-    s: Sphere,
+    shapes: Vec<Box<dyn Shape>>,
 }
 
 fn model(_app: &App) -> Model {
     Model {
-        image: DynamicImage::new_rgb8(WIN_WIDTH as u32, WIN_HEIGHT as u32),
-        fov: 80., // degrees
-        lighting_direction: Vec3::new(0., -1., 0.5).normalize(),
-        s: Sphere {
-            position: Vec3::new(0., 0., 20.),
-            radius: 10.,
-            color: Rgb::<f32>([1., 0.25, 1.]),
-        },
+        image: DynamicImage::new_rgba8(WIN_WIDTH as u32, WIN_HEIGHT as u32),
+        fov: 70., // degrees
+        lighting_direction: Vec3::new(0.5, 1., 0.25).normalize(),
+        shapes: vec![
+            Box::new(Sphere {
+                position: Vec3::new(-2., 0., 20.),
+                radius: 5.,
+                color: [1., 0.25, 1.].into(),
+            }),
+            Box::new(Sphere {
+                position: Vec3::new(2., 0., 10.),
+                radius: 2.,
+                color: [1., 1., 1.].into(),
+            }),
+        ],
     }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
+    // Create fresh image
+    //model.image = DynamicImage::new_rgba8(WIN_WIDTH as u32, WIN_HEIGHT as u32);
+
     let half_win_width = WIN_WIDTH / 2;
     let half_win_height = WIN_HEIGHT / 2;
 
@@ -50,17 +60,29 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
                 ASPECT_RATIO * (model.fov / 2.).tan() * x as f32 / half_win_width as f32,
                 (model.fov / 2.).tan() * y as f32 / half_win_height as f32,
                 1.,
-            );
+            )
+            .normalize();
 
-            let hit_info = ray_sphere_collision(pos, dir, &model.s);
-
-            let color = if let Some(hit) = hit_info {
-                let lightness = (model.lighting_direction.dot(-hit.normal) + 1.) / 2.;
-                let color = hit.color.0.map(|c| (c * lightness * 255.) as u8);
-                Rgba::<u8>([color[0], color[1], color[2], 255])
-            } else {
-                Rgba::<u8>([0, 0, 0, 255])
+            let mut closest_hit = HitInfo {
+                hit_point: Vec3::ZERO,
+                normal: Vec3::ZERO,
+                color: [0., 0., 0.].into(),
+                distance: f32::INFINITY,
             };
+
+            for s in &model.shapes {
+                let hit_info = s.ray_collision(pos, dir);
+
+                if let Some(hit) = hit_info {
+                    if hit.distance < closest_hit.distance {
+                        closest_hit = hit;
+                    }
+                }
+            }
+
+            let lightness = (model.lighting_direction.dot(-closest_hit.normal) + 1.) / 2.;
+            let color = closest_hit.color.0.map(|c| (c * lightness * 255.) as u8);
+            let color = Rgba::<u8>([color[0], color[1], color[2], 255]);
 
             model.image.put_pixel(
                 (x + half_win_width) as u32,
@@ -72,6 +94,7 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
+    frame.clear(BLACK);
     let texture = Texture::from_image(app, &model.image);
     let draw = app.draw();
     draw.texture(&texture).finish();
--- src/ray.rs
+++ src/ray.rs
@@ -4,6 +4,11 @@ pub struct HitInfo {
     pub hit_point: Vec3,
     pub normal: Vec3,
     pub color: Rgb<f32>,
+    pub distance: f32,
+}
+
+pub trait Shape {
+    fn ray_collision(&self, ray_pos: Vec3, ray_dir: Vec3) -> Option<HitInfo>;
 }
 
 pub struct Sphere {
@@ -12,24 +17,27 @@ pub struct Sphere {
     pub color: Rgb<f32>,
 }
 
-pub fn ray_sphere_collision(ray_pos: Vec3, ray_dir: Vec3, sphere: &Sphere) -> Option<HitInfo> {
-    let a = ray_dir.dot(ray_dir);
-    let b = 2. * ray_pos.dot(ray_dir) - 2. * ray_dir.dot(sphere.position);
-    let c = ray_pos.dot(ray_pos) - 2. * ray_pos.dot(sphere.position)
-        + sphere.position.dot(sphere.position)
-        - sphere.radius * sphere.radius;
-    let d = b * b - 4. * a * c;
+impl Shape for Sphere {
+    fn ray_collision(&self, ray_pos: Vec3, ray_dir: Vec3) -> Option<HitInfo> {
+        let a = ray_dir.dot(ray_dir);
+        let b = 2. * ray_pos.dot(ray_dir) - 2. * ray_dir.dot(self.position);
+        let c = ray_pos.dot(ray_pos) - 2. * ray_pos.dot(self.position)
+            + self.position.dot(self.position)
+            - self.radius * self.radius;
+        let d = b * b - 4. * a * c;
 
-    if d < 0. {
-        return None;
+        if d < 0. {
+            return None;
+        }
+
+        let t = (-b - d.sqrt()) / 2. * a;
+        let hit_point = ray_pos + ray_dir * t;
+
+        Some(HitInfo {
+            hit_point,
+            normal: (hit_point - self.position).normalize_or_zero(),
+            color: self.color,
+            distance: t,
+        })
     }
-
-    let t = (-b - d.sqrt()) / 2. * a;
-    let hit_point = ray_pos + ray_dir * t;
-
-    Some(HitInfo {
-        hit_point,
-        normal: (hit_point - sphere.position).normalize_or_zero(),
-        color: sphere.color,
-    })
 }
