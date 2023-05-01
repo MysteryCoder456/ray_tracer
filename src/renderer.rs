6507159268f8834005177aa55202419e455b528b
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-05-01 18:21:15 +0400

    slightly faster rng

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,6 +1,5 @@
 use crate::{shapes::Shape, HitInfo, Scene, WIN_HEIGHT, WIN_WIDTH};
 use nannou::prelude::*;
-use rand::Rng;
 
 const ASPECT_RATIO: f32 = WIN_WIDTH as f32 / WIN_HEIGHT as f32;
 const MAX_RAY_BOUNCES: usize = 3;
@@ -8,7 +7,7 @@ const MAX_RAY_BOUNCES: usize = 3;
 pub fn per_pixel(x: f32, y: f32, scene: &Scene) -> Vec3 {
     let half_win_width = WIN_WIDTH / 2;
     let half_win_height = WIN_HEIGHT / 2;
-    let mut rng = rand::thread_rng();
+    let rng = fastrand::Rng::new();
 
     let mut ray_origin = Vec3::ZERO;
     let mut ray_dir = Vec3::new(
@@ -25,11 +24,8 @@ pub fn per_pixel(x: f32, y: f32, scene: &Scene) -> Vec3 {
         let closest_hit = trace_ray(ray_origin, ray_dir, scene);
 
         if let Some(hit) = closest_hit {
-            let roughness_deviation = Vec3::new(
-                rng.gen_range(-50..50) as f32 / 100.,
-                rng.gen_range(-50..50) as f32 / 100.,
-                rng.gen_range(-50..50) as f32 / 100.,
-            ) * hit.material.roughness;
+            let roughness_deviation =
+                Vec3::new(rng.f32(), rng.f32(), rng.f32()) * 0.5 * hit.material.roughness;
 
             // ray gets reflected about the normal
             ray_origin = hit.hit_point + hit.normal * 0.01;
@@ -81,7 +77,6 @@ fn trace_ray(ray_origin: Vec3, ray_direction: Vec3, scene: &Scene) -> Option<Hit
         if let Some(hit) = hit_info {
             if let Some(ref ch) = closest_hit {
                 if hit.distance < ch.distance {
-                    println!("{:?}", hit.normal);
                     closest_hit = Some(hit);
                 }
             } else {
